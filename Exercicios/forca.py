36a8194ad085ae2e904a908c43c36fe3708e6690
rosnerpn <rosnerti@outlook.com>
2021-02-04 14:10:24 -0300

    Incluido jogo da Forca

---
+++ Exercicios/forca.py
@@ -0,0 +1,32 @@
+def jogar():
+    print('*********************************')
+    print('***Bem vindo ao jogo da Forca!***')
+    print('*********************************')
+    palavra_secreta = 'banana'
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    enforcou = False
+    acertou = False
+    erros = 0
+    print(letras_acertadas)
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        if(chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[posicao] = letra
+                posicao = posicao + 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print('Você ganhou!!')
+    else:
+        print('Você perdeu!!')
+    print('Fim do jogo')
+
+jogar()
